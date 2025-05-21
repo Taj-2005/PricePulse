f5916db3b1599fdd26aa253022d0f111a354274a
Taj-786 <tajuddinshaik786r@gmail.com>
2025-05-21 19:46:53 +0530

    Fixed Code in lib/mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) throw new Error('Please define MONGODB_URI in .env.local');
-
 export async function connectDB() {
-  if (mongoose.connections[0].readyState) return;
-  await mongoose.connect(MONGODB_URI);
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is not defined in environment variables');
+  }
+
+  if (mongoose.connection.readyState >= 1) return;
+
+  return mongoose.connect(MONGODB_URI);
 }
