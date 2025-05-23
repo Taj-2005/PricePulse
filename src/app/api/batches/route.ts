89d19081efb668b97aec44d6d47877f721f06c8c
Taj-786 <tajuddinshaik786r@gmail.com>
2025-05-23 21:08:31 +0530

    Improved UI and added Charts

--- src/app/api/batches/route.ts
+++
@@ -1,8 +0,0 @@
-import { TrackedProduct } from "@/models/trackedProduct";
-
-async function batches(){
-const totalProducts = await TrackedProduct.countDocuments();
-const batchSize = 5;
-const totalBatches = Math.ceil(totalProducts / batchSize);
-console.log(`You need to create ${totalBatches} cron jobs on cron-jobs.org.`);
-}
