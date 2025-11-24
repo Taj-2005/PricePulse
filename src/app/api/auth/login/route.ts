764ba20f6374955b93b51d18ff6d46fc5a088fd8
Taj-786 <tajuddinshaik786r@gmail.com>
2025-11-25 00:43:29 +0530

    added cookies expiry

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -52,6 +52,7 @@ export async function POST(req: Request) {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 7,
       path: "/",
     });
 
